77553ec29beb20e35e07f8e7ece93fcca0ce79f4
Cat-Lon <57090977+Cat-Lon@users.noreply.github.com>
2019-11-04 09:52:52 +0100

    Add files via upload

---
+++ FørsteObjekter/FørsteObjekter/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace FørsteObjekter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Door feats = new Door();
+            feats.closed = false;
+            feats.Color = "gray";
+            feats.Surface = "matte";
+            feats.locked = false;
+            feats.smashed = false;
+
+            Chair cFeats = new Chair();
+            cFeats.Legs = 4;
+            cFeats.Seat = "Cushioned";
+            cFeats.Wheels = true;
+            cFeats.BackRest = "Made of plastic";
+            cFeats.Color = "black";
+
+            Elevator elevator = new Elevator("metal", true, false, false, false, 0);
+
+        }
+    }
+}
