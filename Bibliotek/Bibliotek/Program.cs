22587d0d641bc722be504f6b2f9438cc887702f9
william <william_kammer@live.dk>
2019-11-05 11:23:15 +0100

    Added Library

---
+++ Bibliotek/Bibliotek/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Bibliotek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Library.BorrowBook("The color from space");
+        }
+    }
+}
